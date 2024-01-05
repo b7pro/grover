640d28f65f75a9d4da2e4188d6dd577640dc38ea
b7pro <biennetk@gmail.com>
2024-01-05 09:33:37 +0100

    text

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Header from "./components/header"
+import HomePage from "./components/home"
 
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <Header />
+      <HomePage />
     </>
   )
 }
--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,9 +1,41 @@
+import "./style.css";
+import Strawberry from "../../assets/Strawberry.png"
+import cheff from "../../assets/chef.png"
+
 const HomePage = () => {
-    return ( 
-        <>
-            <h1>page home</h1>
-        </>
-     );
-}
- 
+  return (
+    <>
+      <section className="home-page">
+        <div className="content-home-page">
+          <div className="text-home-page">
+            <div className="link-home-page">
+                <a  href="/">More than Faster</a>
+                <img src={Strawberry} style={{width:"40px"}} alt="" />
+            </div>
+            
+            <h1>
+              Groveries <br />
+              delivered in as <br />little as  <span>2 hours</span>
+            </h1>
+            <p>
+              Grover atssures fresh grocery every morning to your <br />
+              family without getting out in this pandecim
+            </p>
+            <div className="btn-home-page">
+              <button>Order New</button>
+              <i className="bi bi-caret-right-fill"></i>
+              <h3>Order Process</h3>
+            </div>
+          </div>
+          <div className="img-home-page">
+            <div className="img-cheff">
+                
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
 export default HomePage;
