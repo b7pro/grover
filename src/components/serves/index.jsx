7a8beb7eb6f3e7e037ea424a3cfd4c651db8d085
b7pro <biennetk@gmail.com>
2024-01-07 15:03:08 +0100

    done

--- src/components/serves/index.jsx
+++ src/components/serves/index.jsx
@@ -13,11 +13,24 @@ const ServerPage = () => {
           </div>
           <div className="cart-serves">
             <div className="cart">
-              <div className="cart-bg"></div>
                 <i className="bi bi-cash-coin"></i>
                 <div className="text-cart">
-                  <h1>Free Shopping</h1>
-                  <h5>fggkjsghbgfjfbdfhjsdfjgdgjfjgfkgbh</h5>
+                  <h1>Return Money</h1>
+                  <h5>Get your money back at any time</h5>
+                </div>
+            </div>
+            <div className="cart">
+               <i class="bi bi-fingerprint"></i>
+                <div className="text-cart">
+                  <h1>Higher security</h1>
+                  <h5>Payment can be made by card</h5>
+                </div>
+            </div>
+            <div className="cart">
+                <i class="bi bi-shop"></i>
+                <div className="text-cart">
+                  <h1>Shop at any time</h1>
+                  <h5>Shop at any time and from anywhere</h5>
                 </div>
             </div>
           </div>
